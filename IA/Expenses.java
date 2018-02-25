c5df217970373ac2fe3b2f85c595d34f21be4733
Sasha Chang <sasha_chang@Plecostomus.local>
2018-02-25 15:03:04 -0800

    wowee

--- IA/Expenses.java
+++ IA/Expenses.java
@@ -3,32 +3,35 @@ import java.lang.*;
 
 public class Expenses{
   public ArrayList<String> date = new ArrayList<String>();
-  public ArrayList<double> amount = new ArrayList<String>();
+  public ArrayList<Double> amount = new ArrayList<Double>();
   public ArrayList<String> address = new ArrayList<String>();
-  public ArrayList<int> id = new ArrayList<String>();
+  public ArrayList<Integer> id = new ArrayList<Integer>();
   public void setDate(ArrayList<String> date){
     this.date = date;
   }
-  public void setAmount(ArrayList<double> amount){
+  public void setAmount(ArrayList<Double> amount){
     this.amount = amount;
   }
   public void setAddress(ArrayList<String> address){
     this.address = address;
   }
-  public void setId(ArrayList<int> id){
+  public void setId(ArrayList<Integer> id){
     this.id = id;
   }
-  public ArrayList<String> getAddress(){
-    return address;
+  public String getAddress(int a){
+    return address.get(a);
   }
-  public ArrayList<String> getDate(){
-    return date;
+  public String getDate(int a){
+    return date.get(a);
   }
-  public ArrayList<double> getAmount(){
-    return amount;
+  public double getAmount(int a){
+    return amount.get(a);
   }
-  public ArrayList<int> getId(){
+  public ArrayList<Integer> getId(){
     return id;
+  }
+  public int getIdLength(){
+    return id.size();
   }
   public static void main(String[] args){
     System.out.println("Welcome to Expense Tracker \n What would you like to do? Enter \"Add New Expense\" or \"View Expenses\".");
@@ -48,12 +51,12 @@ public class Expenses{
       Scanner scannnn = new Scanner(System.in);
       String amount = scannnn.nextLine();
 
-      id.add(id.length);
+      id.add(getIdLength());
     }
 
     if(a.equals("View Expenses")){
       for(int i = 0; i<id.length; i++){
-        System.println(date.get(i) + " " + address.get(i) + " " + amount.get(i));
+        System.println(getDate(i) + " " + getAddress(i) + " " + getAmount(i));
       }
     }
   }
